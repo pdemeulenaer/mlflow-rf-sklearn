6b9e468663d6072881f7d22e114912471466e533
pdemeulenaer <pdemeulenaer@gmail.com>
2021-05-28 23:46:33 +0300

    adapt code to containarized app 2

---
+++ production/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import pickle
+from numpy import array2string
+
+# define the path to the pickled model
+model_path = "rf-model.pkl"
+
+# unpickle the random forest model
+with open(model_path, "rb") as file:
+    unpickled_rf = pickle.load(file)
+
+# define the app
+app = Flask(__name__)
+
+
+# use decorator to define the /score input method and define the predict function
+@app.route("/score", methods=["POST", "GET"])
+def predict_species():
+    # create list and append inputs
+    flower = []
+    flower.append(request.args.get("petal_length"))
+    flower.append(request.args.get("petal_width"))
+    flower.append(request.args.get("sepal_length"))
+    flower.append(request.args.get("sepal_width"))
+
+    # return the prediction
+    return array2string(unpickled_rf.predict([flower]))
+
+
+# run the app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port="5002")
--- training.py
+++ training.py
@@ -1,8 +1,8 @@
-# Databricks notebook source
-# Declaration of input parameter: the environment selected
-dbutils.widgets.removeAll()
-dbutils.widgets.dropdown("environment", "TEST", 
-                         ["TEST", "SYST", "PROD"], "The environment selected for run")
+# # Databricks notebook source
+# # Declaration of input parameter: the environment selected
+# dbutils.widgets.removeAll()
+# dbutils.widgets.dropdown("environment", "TEST", 
+#                          ["TEST", "SYST", "PROD"], "The environment selected for run")
 
 # COMMAND ----------
 
@@ -13,12 +13,13 @@ import socket
 import traceback
 import pandas as pd
 import numpy as np
-from IPython.core.pylabtools import figsize
+# from IPython.core.pylabtools import figsize
 from matplotlib import pyplot as plt
 import pylab
 from pylab import *
 import matplotlib.cm as cm
 import matplotlib.mlab as mlab
+import pickle
 
 #Import of SKLEARN packages
 from sklearn.metrics import accuracy_score, roc_curve, auc, confusion_matrix
@@ -26,7 +27,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 
-import mlflow
+# import mlflow
 
 # COMMAND ----------
 
@@ -74,19 +75,19 @@ data_conf, model_conf
 # COMMAND ----------
 
 # Define the environment (dev, test or prod)
-env = dbutils.widgets.getArgument("environment")
+# env = dbutils.widgets.getArgument("environment")
 
-print()
-print('Running in ', env)    
+# print()
+# print('Running in ', env)    
 
-data_conf = json.loads(data_json)
+# data_conf = json.loads(data_json)
 model_conf = json.loads(config_json)
 
-print(data_conf[env])
+# print(data_conf[env])
 print(model_conf)
 
 # Define the MLFlow experiment location
-mlflow.set_experiment("/Shared/simple-rf-sklearn/simple-rf-sklearn_experiment")
+# mlflow.set_experiment("/Shared/simple-rf-sklearn/simple-rf-sklearn_experiment")
 
 
 # ---------------------------------------------------------------------------------------
@@ -134,8 +135,8 @@ def train(data_conf, model_conf, **kwargs):
         test_pd.loc[data_pd['label']==0,'species'] = 'setosa'
         test_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
         test_pd.loc[data_pd['label']==2,'species'] = 'virginica'
-        test_df = spark.createDataFrame(test_pd)
-        test_df.write.format("delta").mode("overwrite").save("/mnt/delta/{0}".format('test_data_sklearn_rf'))
+        # test_df = spark.createDataFrame(test_pd)
+        # test_df.write.format("delta").mode("overwrite").save("/mnt/delta/{0}".format('test_data_sklearn_rf'))
 
         print("Step 1.0 completed: Loaded Iris dataset in Pandas")      
 
@@ -150,38 +151,53 @@ def train(data_conf, model_conf, **kwargs):
         # 1.1 Model training
         # ========================================
         
-        with mlflow.start_run() as run:          
+        # with mlflow.start_run() as run:          
 
-            # Model definition
-            max_depth = int(model_conf['hyperparameters']['max_depth'])
-            n_estimators = int(model_conf['hyperparameters']['n_estimators'])
-            max_features = model_conf['hyperparameters']['max_features']
-            criterion = model_conf['hyperparameters']['criterion']
-            class_weight = model_conf['hyperparameters']['class_weight']
-            bootstrap = bool(model_conf['hyperparameters']['bootstrap'])
-            clf = RandomForestClassifier(max_depth=max_depth,
-                                       n_estimators=n_estimators,
-                                       max_features=max_features,
-                                       criterion=criterion,
-                                       class_weight=class_weight,
-                                       bootstrap=bootstrap,
-                                       random_state=21,
-                                       n_jobs=-1)          
-            
-            # Fit of the model on the training set
-            model = clf.fit(x_train, y_train) 
-            
-            # Log the model within the MLflow run
-            mlflow.log_param("max_depth", str(max_depth))
-            mlflow.log_param("n_estimators", str(n_estimators))  
-            mlflow.log_param("max_features", str(max_features))             
-            mlflow.log_param("criterion", str(criterion))  
-            mlflow.log_param("class_weight", str(class_weight))  
-            mlflow.log_param("bootstrap", str(bootstrap))  
-            mlflow.log_param("max_features", str(max_features)) 
-            mlflow.sklearn.log_model(model, 
-                                   "model",
-                                   registered_model_name="sklearn-rf")                        
+        # Model definition
+        max_depth = int(model_conf['hyperparameters']['max_depth'])
+        n_estimators = int(model_conf['hyperparameters']['n_estimators'])
+        max_features = model_conf['hyperparameters']['max_features']
+        criterion = model_conf['hyperparameters']['criterion']
+        class_weight = model_conf['hyperparameters']['class_weight']
+        bootstrap = bool(model_conf['hyperparameters']['bootstrap'])
+        clf = RandomForestClassifier(max_depth=max_depth,
+                                   n_estimators=n_estimators,
+                                   max_features=max_features,
+                                   criterion=criterion,
+                                   class_weight=class_weight,
+                                   bootstrap=bootstrap,
+                                   random_state=21,
+                                   n_jobs=-1,
+                                   oob_score=True)          
+        
+        # Fit of the model on the training set
+        model = clf.fit(x_train, y_train) 
+
+        # score our model and print the output
+        predicted = clf.predict(x_test)
+        accuracy = accuracy_score(y_test, predicted)
+        print(
+            "Out-of-bag score estimate: {0:.3f}\n"
+            "Mean accuracy score: {1:.3f}".format(clf.oob_score_, accuracy
+            )
+        )      
+
+        # Saving the model locally
+        pickle_save = 'rf-model.pkl'
+        with open(pickle_save, 'wb') as file:
+            pickle.dump(clf, file)  
+        
+        # Log the model within the MLflow run
+        # mlflow.log_param("max_depth", str(max_depth))
+        # mlflow.log_param("n_estimators", str(n_estimators))  
+        # mlflow.log_param("max_features", str(max_features))             
+        # mlflow.log_param("criterion", str(criterion))  
+        # mlflow.log_param("class_weight", str(class_weight))  
+        # mlflow.log_param("bootstrap", str(bootstrap))  
+        # mlflow.log_param("max_features", str(max_features)) 
+        # mlflow.sklearn.log_model(model, 
+        #                        "model",
+        #                        registered_model_name="sklearn-rf")                        
 
         print("Step 1.1 completed: model training and saved to MLFlow")                  
 
@@ -202,141 +218,141 @@ if __name__ == "__main__":
 
 # COMMAND ----------
 
-import matplotlib.pyplot as plt  # doctest: +SKIP
-from sklearn.datasets import make_classification
-from sklearn.metrics import plot_confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-X, y = make_classification(random_state=0)
-X_train, X_test, y_train, y_test = train_test_split(
-        X, y, random_state=0)
-clf = SVC(random_state=0)
-clf.fit(X_train, y_train)
+# import matplotlib.pyplot as plt  # doctest: +SKIP
+# from sklearn.datasets import make_classification
+# from sklearn.metrics import plot_confusion_matrix
+# from sklearn.model_selection import train_test_split
+# from sklearn.svm import SVC
+# X, y = make_classification(random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(
+#         X, y, random_state=0)
+# clf = SVC(random_state=0)
+# clf.fit(X_train, y_train)
 
-plot_confusion_matrix(clf, X_test, y_test,cmap='Blues')  # doctest: +SKIP
-plt.show()  # doctest: +SKIP
-plt.savefig('confusion_matrix.png')
+# plot_confusion_matrix(clf, X_test, y_test,cmap='Blues')  # doctest: +SKIP
+# plt.show()  # doctest: +SKIP
+# plt.savefig('confusion_matrix.png')
 
 
 
-# COMMAND ----------
+# # COMMAND ----------
 
-#labels = ['business', 'health']
-cm = confusion_matrix(y_test, y_test)#, labels)
-print(cm)
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.matshow(cm,cmap='Blues')
-plt.title('Confusion matrix of the classifier')
-fig.colorbar(cax)
-#ax.set_xticklabels([''] + labels)
-#ax.set_yticklabels([''] + labels)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.show()
+# #labels = ['business', 'health']
+# cm = confusion_matrix(y_test, y_test)#, labels)
+# print(cm)
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# cax = ax.matshow(cm,cmap='Blues')
+# plt.title('Confusion matrix of the classifier')
+# fig.colorbar(cax)
+# #ax.set_xticklabels([''] + labels)
+# #ax.set_yticklabels([''] + labels)
+# plt.xlabel('Predicted')
+# plt.ylabel('True')
+# plt.show()
 
-# COMMAND ----------
+# # COMMAND ----------
 
-#FEATURES SELECTION
-feature_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-target       = 'label'
+# #FEATURES SELECTION
+# feature_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+# target       = 'label'
 
-def train(clf_name, clf, df_used, feature_cols, target):
+# def train(clf_name, clf, df_used, feature_cols, target):
 
-      X = df_used[feature_cols].values
-      y = df_used[target].values
+#       X = df_used[feature_cols].values
+#       y = df_used[target].values
 
-      # CREATION OF TRAIN-TEST SETS
-      x_train, x_test, y_train, y_test = train_test_split(X,y,train_size=0.7, stratify=y) #stratify=y ensures that the same proportion of labels are in both train and test sets!
+#       # CREATION OF TRAIN-TEST SETS
+#       x_train, x_test, y_train, y_test = train_test_split(X,y,train_size=0.7, stratify=y) #stratify=y ensures that the same proportion of labels are in both train and test sets!
 
-      # THE FIT ON THE TRAINING SET
-      clf.fit(x_train, y_train)
+#       # THE FIT ON THE TRAINING SET
+#       clf.fit(x_train, y_train)
 
-      # THE CLASSIFICATION
-      y_pred = clf.predict(x_test)
+#       # THE CLASSIFICATION
+#       y_pred = clf.predict(x_test)
 
-      # EVALUATION OF THE ACCURACY
-      accuracy = accuracy_score(y_test, y_pred, normalize=True, sample_weight=None)
-      print('Results with the classifier: ', clf_name.upper())
-      print('Accuracy derived (1=100%): ', accuracy)
+#       # EVALUATION OF THE ACCURACY
+#       accuracy = accuracy_score(y_test, y_pred, normalize=True, sample_weight=None)
+#       print('Results with the classifier: ', clf_name.upper())
+#       print('Accuracy derived (1=100%): ', accuracy)
 
-      return y_test, y_pred
+#       return y_test, y_pred
 
-clf_name = 'Random Forest'
-clf = RandomForestClassifier(#max_depth=50,
-                           n_estimators=100,
-                           max_features='auto',
-                           criterion='gini',#'entropy',
-                           class_weight='balanced',
-                           bootstrap=True,
-                           random_state=21,
-                           n_jobs=-1) #n_jobs=-1 uses all available cores!!!
+# clf_name = 'Random Forest'
+# clf = RandomForestClassifier(#max_depth=50,
+#                            n_estimators=100,
+#                            max_features='auto',
+#                            criterion='gini',#'entropy',
+#                            class_weight='balanced',
+#                            bootstrap=True,
+#                            random_state=21,
+#                            n_jobs=-1) #n_jobs=-1 uses all available cores!!!
 
-y_test, y_pred = train(clf_name, clf, data_pd, feature_cols, target)
+# y_test, y_pred = train(clf_name, clf, data_pd, feature_cols, target)
 
-#RANKING OF VARIABLES (available only for Random Forest)
-print("Ranking of variables from Random Forest:")
-feature_importance_index_sorted = np.argsort(clf.feature_importances_)[::-1]
-for jj in feature_importance_index_sorted:
-  print(feature_cols[jj],np.around(clf.feature_importances_[jj],decimals=3)*100,'%')
+# #RANKING OF VARIABLES (available only for Random Forest)
+# print("Ranking of variables from Random Forest:")
+# feature_importance_index_sorted = np.argsort(clf.feature_importances_)[::-1]
+# for jj in feature_importance_index_sorted:
+#   print(feature_cols[jj],np.around(clf.feature_importances_[jj],decimals=3)*100,'%')
 
-# Accuracy and Confusion Matrix
-accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy = ',accuracy)
-#print 'ROC_AUC  = ', roc_auc
-print('Confusion matrix:')
-C = confusion_matrix(y_test, y_pred)
-C_normalized = C / C.astype(np.float).sum()
-#print C_normalized
+# # Accuracy and Confusion Matrix
+# accuracy = accuracy_score(y_test, y_pred)
+# print('Accuracy = ',accuracy)
+# #print 'ROC_AUC  = ', roc_auc
+# print('Confusion matrix:')
+# C = confusion_matrix(y_test, y_pred)
+# C_normalized = C / C.astype(np.float).sum()
+# #print C_normalized
 
-Classes           = ['setosa','versicolor','virginica']
-C_normalized_pd = pd.DataFrame(C_normalized,columns=Classes,index=Classes)
-C_normalized_pd
+# Classes           = ['setosa','versicolor','virginica']
+# C_normalized_pd = pd.DataFrame(C_normalized,columns=Classes,index=Classes)
+# C_normalized_pd
 
-# COMMAND ----------
+# # COMMAND ----------
 
-        # Loading of dataset
-        iris = load_iris()                  #The Iris dataset is available through the scikit-learn API
-        idx = list(range(len(iris.target)))
-        np.random.shuffle(idx)              #We shuffle it (important if we want to split in train and test sets)
-        X = iris.data[idx]
-        y = iris.target[idx]
+#         # Loading of dataset
+#         iris = load_iris()                  #The Iris dataset is available through the scikit-learn API
+#         idx = list(range(len(iris.target)))
+#         np.random.shuffle(idx)              #We shuffle it (important if we want to split in train and test sets)
+#         X = iris.data[idx]
+#         y = iris.target[idx]
 
-        # Load data in Pandas dataFrame
-        data_pd = pd.DataFrame(data=np.column_stack((X,y)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
-        data_pd.loc[data_pd['label']==0,'species'] = 'setosa'
-        data_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
-        data_pd.loc[data_pd['label']==2,'species'] = 'virginica'
-        data_pd.head()
+#         # Load data in Pandas dataFrame
+#         data_pd = pd.DataFrame(data=np.column_stack((X,y)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
+#         data_pd.loc[data_pd['label']==0,'species'] = 'setosa'
+#         data_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
+#         data_pd.loc[data_pd['label']==2,'species'] = 'virginica'
+#         data_pd.head()
         
-        # Feature selection
-        feature_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-        target       = 'label'   
+#         # Feature selection
+#         feature_cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+#         target       = 'label'   
         
-        X = data_pd[feature_cols].values
-        y = data_pd[target].values
+#         X = data_pd[feature_cols].values
+#         y = data_pd[target].values
 
-        # Creation of train and test datasets
-        x_train, x_test, y_train, y_test = train_test_split(X,y,train_size=0.7, stratify=y) #stratify=y ensures that the same proportion of labels are in both train and test sets! 
+#         # Creation of train and test datasets
+#         x_train, x_test, y_train, y_test = train_test_split(X,y,train_size=0.7, stratify=y) #stratify=y ensures that the same proportion of labels are in both train and test sets! 
         
-        test_pd = pd.DataFrame(data=np.column_stack((x_test,y_test)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
-        test_pd.loc[data_pd['label']==0,'species'] = 'setosa'
-        test_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
-        test_pd.loc[data_pd['label']==2,'species'] = 'virginica'
-        test_df = spark.createDataFrame(test_pd)
-        test_df.write.format("delta").mode("overwrite").save("/mnt/delta/{0}".format('test_data_sklearn_rf'))
+#         test_pd = pd.DataFrame(data=np.column_stack((x_test,y_test)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
+#         test_pd.loc[data_pd['label']==0,'species'] = 'setosa'
+#         test_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
+#         test_pd.loc[data_pd['label']==2,'species'] = 'virginica'
+#         test_df = spark.createDataFrame(test_pd)
+#         test_df.write.format("delta").mode("overwrite").save("/mnt/delta/{0}".format('test_data_sklearn_rf'))
 
-# COMMAND ----------
+# # COMMAND ----------
 
-        test_pd = pd.DataFrame(data=np.column_stack((x_test,y_test)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
-        test_pd.loc[data_pd['label']==0,'species'] = 'setosa'
-        test_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
-        test_pd.loc[data_pd['label']==2,'species'] = 'virginica'
-        test_pd.head()
+#         test_pd = pd.DataFrame(data=np.column_stack((x_test,y_test)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
+#         test_pd.loc[data_pd['label']==0,'species'] = 'setosa'
+#         test_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
+#         test_pd.loc[data_pd['label']==2,'species'] = 'virginica'
+#         test_pd.head()
 
-# COMMAND ----------
+# # COMMAND ----------
 
-X = data_pd[feature_cols].values
-X
+# X = data_pd[feature_cols].values
+# X
 
-# COMMAND ----------
+# # COMMAND ----------
