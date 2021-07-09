27a3b1990b7f03de97e732ad972406ad5a06f50c
pdemeulenaer <pdemeulenaer@gmail.com>
2021-07-09 17:44:48 +0300

    new data generated at each request

--- data_generation/data_generation.py
+++ data_generation/data_generation.py
@@ -11,24 +11,28 @@ import io
 # 1. Full Dataset Loading
 # ==============================
 # Loading of dataset
-iris = load_iris()                  #The Iris dataset is available through the scikit-learn API
-idx = list(range(len(iris.target)))
-np.random.shuffle(idx)              #We shuffle it (important if we want to split in train and test sets)
-X = iris.data[idx]
-y = iris.target[idx]
-# Load data in Pandas dataFrame
-data_pd = pd.DataFrame(data=np.column_stack((X,y)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
-data_pd.loc[data_pd['label']==0,'species'] = 'setosa'
-data_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
-data_pd.loc[data_pd['label']==2,'species'] = 'virginica'
-data_pd.head()
+
+def get_dataframe_from_somewhere(N):
+    iris = load_iris()                  #The Iris dataset is available through the scikit-learn API
+    idx = list(range(len(iris.target)))
+    np.random.shuffle(idx)              #We shuffle it (important if we want to split in train and test sets)
+    X = iris.data[idx]
+    y = iris.target[idx]
+    # Load data in Pandas dataFrame
+    data_pd = pd.DataFrame(data=np.column_stack((X,y)), columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'label'])
+    data_pd.loc[data_pd['label']==0,'species'] = 'setosa'
+    data_pd.loc[data_pd['label']==1,'species'] = 'versicolor'
+    data_pd.loc[data_pd['label']==2,'species'] = 'virginica'
+    data_pd.head()
+
+    # N=30
+    #data_gen_pd = data_pd[:N]
+    return data_pd[:N]
 
 
 
 # define the app
 app = Flask(__name__)
-
-
 
 # @app.route('/api/')
 # def makecalc():
@@ -37,14 +41,13 @@ app = Flask(__name__)
 #     # resp.headers["Content-Type"] = "text/csv"
 #     return resp
 
-
 @app.route("/api")
 def dfjson():
     """
     return a json representation of the dataframe
     """
-    # df = get_dataframe_from_somewhere()
-    return Response(data_pd.to_json(orient="records"), mimetype='application/json')    
+    df = get_dataframe_from_somewhere(N=30)
+    return Response(df.to_json(orient="records"), mimetype='application/json')    
 
 # run the app
 if __name__ == "__main__":
